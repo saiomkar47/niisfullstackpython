77cf902ad49d7adef37676f065d9d3ff431188be
sai omkar <mr.saiomkar113@gmail.com>
2026-02-21 21:18:55 +0530

    added a new file

---
+++ basic/basic1.py
@@ -0,0 +1,11 @@
+#wap take rectangle length and breadth from keyword disply length breadth area and parimeter..
+print("enter rectangular length:")
+L=int(input())
+print("enter rectangular breadth:")
+B=int(input())
+ar=L*B
+pr=2*(L+B)
+print("length=",L)
+print("breadth=",B)
+print("area=",ar)
+print("parimeter=",pr)
---
+++ basic/basic2.py
@@ -0,0 +1,7 @@
+print("enter squre side:")
+L=int(input())
+ar=L*L
+pr=4*L
+print("length=",L)
+print("area=",ar)
+print("parimeter=",pr)
